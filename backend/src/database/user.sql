95e68b6e5576e7e8b487455bdd5821031d1e9643
Valentine Ezeh <32917511+valentineezeh@users.noreply.github.com>
2024-02-05 09:01:26 +0100

    make email unique

--- backend/src/database/user.sql
+++ backend/src/database/user.sql
@@ -2,6 +2,6 @@ CREATE TABLE CryptoUsers (
     id SERIAL PRIMARY KEY,
     firstName VARCHAR(100) NOT NULL,
     lastName VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) unique NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
